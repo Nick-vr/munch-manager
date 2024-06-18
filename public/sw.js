dff5ae6671747868eaad93135f6da7e0edd7c657
Nick-vr <nvanroyen@gmail.com>
2024-06-18 22:50:58 +0200

    added own sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,41 @@
-// Fill file
+const CACHE_NAME = 'munch-manager-cache-v1'
+const ASSETS_TO_CACHE = [
+  '/',
+  '/manifest.json',
+  '/mm-192x192.png',
+  '/mm-512x512.png',
+  // Add other assets you want to cache
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE)
+    })
+  )
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    })
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response
+      }
+      return fetch(event.request)
+    })
+  )
+})
